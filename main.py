d555bda5d1cbd00a6dc331642c6dfd4b18ac8702
funs <pc904b28@yahoo.com.tw>
2018-06-04 01:37:11 +0800

    pause scanning

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from camspy import netSurveillance
 
 def scan(subnet2):
-    for subnet3 in range(0,256,1):
+    for subnet3 in range(67,256,1):
         netSurveillance.subnetScan(subnet2,subnet3)
         
 if __name__ == '__main__':
